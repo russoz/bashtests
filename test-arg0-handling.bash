06c97d1100630b4854605f7c4b51f0313f84361d
Alexei Znamensky <russoz@cpan.org>
2013-01-11 10:44:37 -0200

    testing handling of the $0 argument to extract file and dir
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ test-arg0-handling.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+p() {
+	C="$1"
+	V=$(eval echo \$$C)
+	echo "${C}=(${V})" >&2
+}
+
+FILE=${0##*/}
+ARG0DOT="./$0"
+DIR=${ARG0DOT%/*}
+DIRN=$( (cd $DIR; pwd) )
+
+p "FILE"
+p "DIR"
+p "DIRN"
